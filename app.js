81b1f9ffb30b8ff9e3990bf881e7a9d36fad082d
Nick Doiron <ndoiron@mapmeld.com>
2013-09-25 11:55:57 -0400

    add IE compatibility

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var app = express();
 
 app.get('/', function(request, response) {
     response.sendfile(__dirname + '/index.html');
+})
+app.get('/index2.html', function(request, response) {
+    response.sendfile(__dirname + '/index2.html');
 }).configure(function() {
     app.use('/static', express.static(__dirname + '/static'));
 }).listen(port);
