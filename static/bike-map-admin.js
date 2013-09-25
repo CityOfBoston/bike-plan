ceea6bbd57df6534340d4c74c5e941380fcd6139
Nick Doiron <ndoiron@mapmeld.com>
2013-09-25 11:58:31 -0400

    add IE compatibility

--- static/bike-map-admin.js
+++ static/bike-map-admin.js
@@ -749,8 +749,6 @@ function credits(){
 }
 
 function isIE(){
-  return 7;
-
   var myNav = navigator.userAgent.toLowerCase();
   return (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : false;
 }
